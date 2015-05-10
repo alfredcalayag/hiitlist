940c91ab097dfd4005c4c2c8e917a72783ceeee8
alfredcalayag <rapadura05@gmail.com>
2015-05-09 22:54:03 -0700

    Added user model tests and User model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  # Add additional email validation using Regex awesomeness
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,9 +6,28 @@ describe User do
     expect(user).to be_valid
   end
 
-  it "is invalid without a name"
-  it "is invalid without an email"
-  it "is invalid with a duplicate email address (i.e. uniqueness)"
-  it "returns a user's name as a string"
+  it "is invalid without a name" do
+    user = User.new(email: "something@you.com")
+    user.valid?
+    expect(user.errors[:name]).to include("can't be blank")
+  end
+
+  it "is invalid without an email" do
+    user = User.new(name: "myname?")
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
+  end
+
+  it "is invalid with a duplicate email address (i.e. uniqueness)" do
+    User.create(name: "Joe One", email: "joe@one.com")
+    user = User.new(name: "Joe Two", email: "joe@one.com")
+    user.valid?
+    expect(user.errors[:email]).to include("has already been taken")
+  end
+  it "returns a user's name as a string" do
+    User.create(name: "Joe", email:"joe@one.com")
+    user = User.last
+    expect(user.name).to be_instance_of(String)
+  end
 end
 
