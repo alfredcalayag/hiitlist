d34e2913276477ec586335583b6bc4abc66f77c8
alfredcalayag <rapadura05@gmail.com>
2015-05-10 20:03:48 -0700

    Added more users, lists, and exercises to seed data

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150510014336) do
+ActiveRecord::Schema.define(version: 20150510225830) do
+
+  create_table "exercises", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "list_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "exercises", ["list_id"], name: "index_exercises_on_list_id"
 
   create_table "lists", force: :cascade do |t|
     t.string   "name"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -6,6 +7,18 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-user = User.create(name: "John Doe", email: "john@doe.com")
+# Create 10 users
+(1..10).to_a.each do |num|
+  user = User.create(name: Faker::Name.name, email: Faker::Internet.email)
 
-List.create(name: "My Workout List", user_id: user.id)
+  # Create 3 lists for each user
+  (1..3).to_a.each do |list_count|
+    list = List.create(name: Faker::Company.name, user_id: user.id)
+
+    # Create 6 exercises for each list
+    (1..6).to_a.each do |count|
+      Exercise.create(name: Faker::Lorem.sentence(3), list_id: list.id)
+    end
+  end
+
+end
