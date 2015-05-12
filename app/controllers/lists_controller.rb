f28d55df257b445ac9ce10ca99d866d2a7570f71
alfredcalayag <rapadura05@gmail.com>
2015-05-12 02:27:47 -0700

    Added user create routes. Adjusted list routes.

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,4 +3,16 @@ class ListsController < ApplicationController
     @list = List.find(params[:id])
     @exercises = Exercise.where(list_id: @list.id)
   end
+
+  def create
+    @user = User.find(params[:user_id])
+    @list = List.new(list_params)
+    @list.save
+    redirect_to @user
+  end
+
+  private
+  def list_params
+    params.require(:list).permit(:name, :user_id)
+  end
 end
