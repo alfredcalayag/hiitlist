56560063196c87059abb6965d7f57bdeb245e340
alfredcalayag <rapadura05@gmail.com>
2015-05-12 00:49:10 -0700

    Clean up and improved timer accuracy

--- app/assets/javascripts/timer.js
+++ app/assets/javascripts/timer.js
@@ -9,8 +9,6 @@ window.onload = function() {
   // var resume = document.getElementById("resume");
   var id;
   var value = "00:00";
-
-  // Initialize the timer value
   timer.innerHTML = value;
 
   // Grab the list of exercises from the page
@@ -18,98 +16,91 @@ window.onload = function() {
 
   $('#current_exercise').text("READY TO WORK?");
 
+  var exArray = ['get ready...', 'jumps', 'rest', 'squats','rest','kicks', 'rest', 'punches'];
+  var index = 0;
 
-newTimer = function(duration) {
+  beginWorkout = function(highTime) {
 
-  secondPassed = function() {
-      var minutes = Math.round((duration - 30)/60);
-      var remainingSeconds = duration % 60;
-      if (remainingSeconds < 10) {
-          remainingSeconds = "0" + remainingSeconds;
-      }
-      timer.innerHTML = "0" + minutes + ":" + remainingSeconds;
-      if (duration <= 0) {
-          clearInterval(countdownInterval);
-          console.log("Time complete!");
-          return;
-      } else {
-          duration--;
-      }
-  }
-  var countdownInterval = setInterval(secondPassed, 1000);
-
-}
-
-
-  var one = function(description, seconds){
-    var deferred = $.Deferred();
-    var timeBuffer = seconds * 1000 + 1500;
-    newTimer(seconds);
-
-    $('#current_exercise').text(description);
-
-    setTimeout(function(){
-      console.log('one');
-      deferred.resolve();
-    }, timeBuffer); //allows newTimer to complete
-
-      return deferred.promise();
-  }
-
-
-  // var deferreds = [];
-  // for (var i = 0; i < collection.length; i++){
-  //   return function(){
-  //     deferreds.push(one);
-  //   }
-  // }
-
-
-  begin = function(){
-    // three().then(three);
-    one("First exercise", 5).then(one("Second exercise", 9));
-  }
-
-var exArray = ['jumps','squats', 'kicks', 'punches'];
-var index = 0;
-
-beginWorkout = function() {
-  $('#current_exercise').text("Get Ready..");
-
-  var duration = 5;
+  var duration = highTime;
 
   function nextExercise() {
-    $('#current_exercise').text(exArray[index]);
     index++;
-    // newTimer(5);
-    duration = 5;
+    duration = highTime;  //
     if (index > exArray.length) {
       clearInterval(exerciseInterval);
       clearInterval(countdownInterval);
+      timer.innerHTML = "00:00";
+      $('#current_exercise').text("Workout Complete!");
       console.log("Exercise completed!");
       return;
     }
   }
 
-  secondPassed = function() {
+  function secondPassed() {
+      if (duration == highTime) {
+        $('#current_exercise').text(exArray[index]);
+        console.log("BUZZER NOISE");
+      }
+
       var minutes = Math.round((duration - 30)/60);
       var remainingSeconds = duration % 60;
       if (remainingSeconds < 10) {
           remainingSeconds = "0" + remainingSeconds;
       }
       timer.innerHTML = "0" + minutes + ":" + remainingSeconds;
-      // if (duration < 0) {
-      //     clearInterval(countdownInterval);
-      //     console.log("Exercise completed!");
-      //     return;
-      // } else {
-          duration--;
-      // }
+      duration--;
   }
-  var countdownInterval = setInterval(secondPassed, 1000);
-  var exerciseInterval = setInterval(nextExercise, 5500);
+
+  var countdownInterval = setInterval(secondPassed, 1000); // 1 second
+  var exerciseInterval = setInterval(nextExercise, duration*1000 + 600);
+
+}
 
 
 }
 
-}
+// ========= REFERENCE SECTION ============
+
+// Deprecated timer example.  For reference only.
+// newTimer = function(duration) {
+
+//   secondPassed = function() {
+//       var minutes = Math.round((duration - 30)/60);
+//       var remainingSeconds = duration % 60;
+//       if (remainingSeconds < 10) {
+//           remainingSeconds = "0" + remainingSeconds;
+//       }
+//       timer.innerHTML = "0" + minutes + ":" + remainingSeconds;
+//       if (duration <= 0) {
+//           clearInterval(countdownInterval);
+//           console.log("Time complete!");
+//           return;
+//       } else {
+//           duration--;
+//       }
+//   }
+//   var countdownInterval = setInterval(secondPassed, 1000);
+
+// }
+
+
+// Deprecated promise function example.  For reference only.
+  // var one = function(description, seconds){
+  //   var deferred = $.Deferred();
+  //   var timeBuffer = seconds * 1000 + 1500;
+  //   newTimer(seconds);
+
+  //   $('#current_exercise').text(description);
+
+  //   setTimeout(function(){
+  //     console.log('one');
+  //     deferred.resolve();
+  //   }, timeBuffer); //allows newTimer to complete
+
+  //     return deferred.promise();
+  // }
+
+  // begin = function(){
+  //   // three().then(three);
+  //   one("First exercise", 5).then(one("Second exercise", 9));
+  // }
