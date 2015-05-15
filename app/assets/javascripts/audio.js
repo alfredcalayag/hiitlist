4602cb79bd57c776b894d8ee673c02d55191670a
alfredcalayag <rapadura05@gmail.com>
2015-05-14 17:39:20 -0700

    Trial solution for sound issue on mobile browsers

--- app/assets/javascripts/audio.js
+++ app/assets/javascripts/audio.js
@@ -2,4 +2,9 @@ $(document).ready(function(){
   console.log("Bell Loaded");
   bellSound = document.getElementById('bell');
   gong = document.getElementById('gong');
+
+  bellSound.play();
+  bellSound.pause();
+  gong.play();
+  gong.pause();
 });
