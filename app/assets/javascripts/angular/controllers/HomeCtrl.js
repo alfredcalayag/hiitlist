e4c4de2aa7e60afb57ffa92541d8102216bead64
alfredcalayag <rapadura05@gmail.com>
2015-06-28 15:10:01 -0700

    Add create user and create workout functionality

--- app/assets/javascripts/angular/controllers/HomeCtrl.js
+++ app/assets/javascripts/angular/controllers/HomeCtrl.js
@@ -1,6 +1,20 @@
 // HomeCtrl.js
 myApp.controller('HomeCtrl', ['$scope', '$location', '$http', function($scope, $location, $http) {
 
+
+  $scope.createWorkout = function(listData){
+    $http.post("../users/" + $scope.userId + "/lists/", {user_id: $scope.userId, name: listData.name})
+      .success(function(data){
+        console.log("Successfully created a new workout list");
+        console.log(data);
+        $scope.lists = data.lists;
+        $scope.newWorkout.name = "";
+      })
+      .error(function(){
+        console.log("Failed to create user.");
+      });
+  }
+
   $scope.showList = function(listId) {
     $location.path('/list').search({id: listId});
   }
--- app/assets/javascripts/angular/controllers/IndexCtrl.js
+++ app/assets/javascripts/angular/controllers/IndexCtrl.js
@@ -2,6 +2,21 @@
 
 myApp.controller('IndexCtrl', ['$scope', '$location', '$http', function($scope, $location, $http) {
 
+  $scope.createUser = function(user){
+    $http.post("../users/", {user})
+      .success(function(data){
+        console.log("Successfully created a new user");
+        console.log(data);
+        $scope.users = data.users;
+        $scope.newUser.name = "";
+        $scope.newUser.email = "";
+        $scope.createUserForm;
+      })
+      .error(function(){
+        console.log("Failed to create user.");
+      });
+  }
+
   $scope.userHome = function(userId) {
     $location.path('/home').search({id: userId});
   }
