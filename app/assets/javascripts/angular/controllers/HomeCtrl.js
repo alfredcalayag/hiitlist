b395f4e25cd1e65d055a8afa7d5a0f6aa1c52d0f
alfredcalayag <rapadura05@gmail.com>
2015-07-04 10:37:00 -0700

    Moved get user http request to only invoke under a conditional situation

--- app/assets/javascripts/angular/controllers/HomeCtrl.js
+++ app/assets/javascripts/angular/controllers/HomeCtrl.js
@@ -5,14 +5,16 @@ myApp.controller('HomeCtrl', ['$scope', '$http', '$state', '$stateParams', '$coo
     $state.go('signin');
   }
 
-  // Get user data
-  $scope.userId = $cookies.currentUserId;
-  $http.get("../users/" + $scope.userId).success(function(response, body){
-    $scope.userName = response.user.name;
-    $scope.lists = response.lists;
-  }).error(function() {
-    console.log("Failed to load user data.");
-  });
+  else {
+    // Get user data
+    $scope.userId = $cookies.currentUserId;
+    $http.get("../users/" + $scope.userId).success(function(response, body){
+      $scope.userName = response.user.name;
+      $scope.lists = response.lists;
+    }).error(function() {
+      console.log("Failed to load user data.");
+    });
+  }
 
   $scope.createWorkout = function(listData){
     $http.post("../users/" + $scope.userId + "/lists/", {user_id: $scope.userId, name: listData.name})
